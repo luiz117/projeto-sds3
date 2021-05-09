7f6740d208bb9dbcdf684b90e7331e965725d40e
Luiz Eduardo Silva <luiz1082@hotmail.com>
2021-05-09 01:09:44 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+
+
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/luiz117" target="_blank" rel="noreferrer">Luiz Eduardo Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+
+
+    );
+}
+
+export default Footer;
