183daaa7a75938979abf97b9ba9126d2c5580b2f
Luiz Eduardo Silva <luiz1082@hotmail.com>
2021-05-09 21:01:30 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,11 +1,38 @@
-export type SaleSum =  {
+import { type } from "os"
+import { SelectHTMLAttributes } from "react"
+import { Seller } from "./seller"
+
+export type Sale = {
+
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string
+    seller: Seller;
+
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;    
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
+export type SaleSum = {
     sellerName: string;
-    sum: number; 
+    sum: number;
 }
 
 export type SaleSuccess = {
-    
-        sellerName: string;
-        visited: number;
-        deals: number;
-    }
+
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
